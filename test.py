306beccfb0de3f315ee230841efa18c4f6977859
Andrei Savu <savu.andrei@gmail.com>
2009-07-17 12:14:28 +0300

    basic test for card value parser

--- test.py
+++ test.py
@@ -23,7 +23,29 @@ class TestCardColor(unittest.TestCase):
             thrown = True
         self.assertTrue(thrown)
         
-        
+class TestCardValue(unittest.TestCase):
+
+    def testParse(self):
+        for x in range(2,10):
+            c = CardValue(str(x))
+            self.assertEquals(c.value, str(c)) 
+        data = {
+            'T' : 'Ten',
+            'J' : 'Jack',
+            'Q' : 'Queen',
+            'K' : 'King',
+            'A' : 'Ace'}
+        for k in data.keys():
+            c = CardValue(str(k))
+            self.assertEquals(str(c), str(c))
+
+    def testParseFail(self):
+        thrown = False
+        try:
+            c = CardValue('X')
+        except ValueError:
+            thrown = True
+        self.assertTrue(thrown)
 
 if __name__ == '__main__':
     unittest.main()
