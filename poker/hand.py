d93d93d2b539149376797e164e034e5b5c97c9d0
Andrei Savu <savu.andrei@gmail.com>
2009-08-03 14:13:53 +0300

    failing test: comparing royal flush with straight flush

--- poker/hand.py
+++ poker/hand.py
@@ -12,18 +12,22 @@ class Hand:
     
     name = property(lambda self:self._name)
     cards = property(lambda self:self._cards[:])
+    score = property(lambda self:self.get_score())
 
     def __init__(self, cards, name=''):
         if len(cards) != Hand.MAX_SIZE:
             raise ValueError("A hand should contain %d cards" % Hand.MAX_SIZE)
 
         self._cards = []
+        self._score = None
+        self._name = name
+
         for card in cards:
             c = Card(card)
             if c in self._cards:
                 raise ValueError('Duplicate card in one hand')
             self._cards.append(c)
-        self._name = name
+
 
     def contains_any(self, hand):
         for card in hand.cards:
@@ -34,5 +38,11 @@ class Hand:
     def get_highest_card(self):
         return sorted(self.cards)[-1]
 
-    
+    def get_score(self):
+        if self._score is None:
+            self._score = self._compute_score()
+        return self._score
+
+    def _compute_score(self):
+        return 0
 
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -4,6 +4,18 @@ from poker import *
 
 class TestHand(unittest.TestCase):
 
+    def setUp(self):
+        self.royal_flush = Hand(['TS', 'JS', 'QS', 'KS', 'AS'])
+        self.straight_flush = Hand(['7D', '8D', '9D', 'TD', 'JD'])
+        self.four_of_a_kind = Hand(['6H', 'JS', 'JD', 'JH', 'JC'])
+        self.full_house = Hand(['QH', 'QS', 'KD', 'KC', 'KH'])
+        self.flush = Hand(['AC', '5C', '7C', '9C', 'JC'])
+        self.straight = Hand(['8H', '9C', 'TC', 'JD', 'QS'])
+        self.three_of_a_kind = Hand(['7H', 'TC', 'KH', 'KD', 'KS'])
+        self.two_pair = Hand(['4H', 'JS', 'JH', 'QC', 'QD'])
+        self.pair = Hand(['2D', '6H', '7D', 'TS', 'TC'])
+        self.nothing = Hand(['4H', '6S', '8C', 'TD', 'QH']) 
+
     def testParse(self):
         h = Hand(['2H', '3D', '5S', '9C', 'KD'])
 
@@ -55,3 +67,7 @@ class TestHand(unittest.TestCase):
         c = Card('KD')
         h = Hand(['2H', '3D', '5S', '9C', str(c)])
         self.assertEquals(c, h.get_highest_card())
+
+    def testScore(self):
+        self.assertTrue(self.royal_flush.score > self.straight_flush.score)
+
