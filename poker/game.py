6ac9ff01d9636eae38e26de524883f0fc467700f
Andrei Savu <savu.andrei@gmail.com>
2009-08-05 07:48:27 +0300

    get game winning hand

--- poker/game.py
+++ poker/game.py
@@ -10,3 +10,15 @@ class Game:
                 raise ValueError('Duplicate card not allowed')
         self.hands.append(hand)
     
+    def get_winner(self):
+        """ Find the winning hand. Return None if the game is tie. """
+        if len(self.hands) == 0:
+            raise Exception('This game has no players')
+        if len(self.hands) == 1:
+            return self.hands[0]
+        
+        self.hands.sort(reverse=True)
+        if self.hands[0] == self.hands[1]:
+            return None
+        return self.hands[0]
+        
--- tests/test_game.py
+++ tests/test_game.py
@@ -4,6 +4,34 @@ from poker import *
 
 class TestGame(unittest.TestCase):
 
+    def setUp(self):
+        self.white_wins = [
+            (['TS', 'JS', 'QS', 'KS', 'AS'], 'White'),
+            (['7D', '8D', '9D', 'TD', 'JD'], 'Black')
+        ]
+        self.tie = [
+            (['3D', '4H', '5D', 'TS', 'TC'], 'White'),
+            (['2D', '6H', '7D', 'TD', 'TH'], 'Black')
+        ]
+
+    def createGame(self, cfg):
+        g = Game()
+        for hand, name in cfg:
+            g.add_hand(Hand(hand,name))
+        return g
+
+    def getWinner(self, cfg):
+        g = self.createGame(cfg)
+        return g.get_winner()
+
+    def testGetWinner(self):
+        h = self.getWinner(self.white_wins)
+        self.assertEquals(h.name, 'White')
+        
+    def testGetWinner_Tie(self):
+        h = self.getWinner(self.tie)
+        self.assertEquals(h, None)
+
     def testAddHand(self):
         valid_hands = [
             ['2H', '3D', '5S', '9C', 'KD'], 
@@ -27,6 +55,6 @@ class TestGame(unittest.TestCase):
             self.assertTrue(False)
         except ValueError:
             pass
-
         
+    
  
