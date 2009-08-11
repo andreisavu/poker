ccf60c5a0cdc4867fcfadbed20760e1c523197b2
Andrei Savu <savu.andrei@gmail.com>
2009-08-11 22:39:43 +0300

    two pair check

--- poker/checks.py
+++ poker/checks.py
@@ -126,8 +126,25 @@ class ThreeOfAKindChecker(_HandChecker):
         return self._offset
 
 class TwoPairChecker(_HandChecker):
-    def match(self, hand): return False
-    def offset(self): return 0
+
+    def __init__(self):
+        self._offset = 0
+
+    def match(self, hand):
+        counts = hand.counts_by_value()
+        count, max = 0, 0
+        for k, v in counts.items():
+            if v == 2:
+                count += 1
+                if max < k:
+                    max = k
+        if count == 2:
+            self._offset = max
+            return True
+        return False
+
+    def offset(self):
+        return self._offset
 
 class PairChecker(_HandChecker):
     def match(self, hand): return False
--- tests/test_checks.py
+++ tests/test_checks.py
@@ -53,5 +53,10 @@ class TestCheckers(unittest.TestCase):
         self.assertTrue(check.match(self.three_of_a_kind))
         self.assertFalse(check.match(self.straight_flush))
 
+    def testTwoPair(self):
+        check = TwoPairChecker()
+        self.assertTrue(check.match(self.two_pair))
+        self.assertFalse(check.match(self.straight_flush))
+
               
 
